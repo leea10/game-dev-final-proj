043d070f39eea5b357fa00af329bf2f97d1598de
MinhKPham <admin@halcyon-363.dynamic.rpi.edu>
2016-11-29 19:27:24 -0500

    Rotate or st

--- JonathanOatesIndividualWork/3rd Person Controller/Assets/Scripts/WASD_Controls.cs
+++ JonathanOatesIndividualWork/3rd Person Controller/Assets/Scripts/WASD_Controls.cs
@@ -13,6 +13,13 @@ public class WASD_Controls : MonoBehaviour {
 	float startingRotationY;
 	float startingPositionY;
 	float startingRotationX;
+	public float speedH = 4.0f;
+	public float speedV = 4.0f;
+	public float sp=0.1f;
+	private float yaw = 0.0f;
+	private float pitch = 0.0f;
+	public Object t;
+	private int i = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -85,6 +92,23 @@ public class WASD_Controls : MonoBehaviour {
 				cameraBase.localEulerAngles.y,
 				cameraBase.localEulerAngles.z);
 		}
+		Screen.lockCursor = true;
+		float spawnDistance = 10;
+
+		Vector3 spawnPos = transform.position + transform.forward*spawnDistance;
+
+
+		yaw += speedH * Input.GetAxis("Mouse X");
+		pitch -= speedV * Input.GetAxis("Mouse Y");
+		if (pitch <= -90) {
+			pitch = -90;
+		}
+		if (pitch >= 90) {
+			pitch = 90;
+		}
+
+
+		transform.eulerAngles = new Vector3(0, yaw, 0.0f);
 	}
 
 	void FixedUpdate () {
