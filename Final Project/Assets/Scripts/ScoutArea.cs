c3573c260ff00da9dad2d60b9c0f596fdf0d093d
Ariel Lee <ariellee616@gmail.com>
2016-12-02 15:43:00 -0500

    Started event delegate for scout area

---
+++ Final Project/Assets/Scripts/ScoutArea.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoutArea : MonoBehaviour {
+	public string areaName;
+
+	public delegate void PlayerCollideAction(string areaName);
+	public static event PlayerCollideAction OnPlayerCollide;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "PlayerModel") {
+			Debug.Log ("Player collided with " + areaName);
+			if (OnPlayerCollide != null) {
+				OnPlayerCollide (areaName);
+			} else {
+				Debug.Log ("No event delegates yet!");
+			}
+		}
+	}
+}
