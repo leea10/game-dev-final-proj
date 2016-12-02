225c514976ed6458c6dbaf4e6007f3b23e7c3f80
Ariel Lee <ariellee616@gmail.com>
2016-12-02 15:00:13 -0500

    Reverted a change to playermodel to prevent merge conflict

--- Final Project/Assets/Scripts/ScoutAreaTrigger.cs
+++ Final Project/Assets/Scripts/ScoutAreaTrigger.cs
@@ -2,16 +2,9 @@
 using System.Collections;
 
 public class ScoutAreaTrigger : MonoBehaviour {
-
 	public string areaName;
-
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnTriggerEnter(Collider other) {
+		Debug.Log (areaName);
 	}
 }
