d09e6d3abcb345a85ead30bf8c8cfff67e891fc9
Ariel Lee <ariellee616@gmail.com>
2016-12-03 11:02:47 -0500

    Starte level manager that currently detects when player enters a scout area

---
+++ Final Project/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	void Start () {
+		// Find all scout areas in the level.
+	}
+
+	void OnEnable () {
+		// Event Listeners.
+		ScoutArea.OnPlayerCollide += TestPrint; // Player enters a scout area.
+	}
+
+	void OnDisable () {
+		// Stop listening to events.
+		ScoutArea.OnPlayerCollide -= TestPrint;
+	}
+
+	// Event handler for when player enters a scout area.
+	void TestPrint (string s) {
+		Debug.Log ("Level manager detected that player collided with " + s);
+	}
+}
