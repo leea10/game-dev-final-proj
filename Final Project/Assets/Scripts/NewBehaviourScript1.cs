6b3a1b9b967bc450e9219b4a9b7896aafe3072c3
MinhKPham <phamm@rpi.edu>
2016-12-06 17:34:10 -0500

    Update NewBehaviourScript1.cs

--- Final Project/Assets/Scripts/NewBehaviourScript1.cs
+++ Final Project/Assets/Scripts/NewBehaviourScript1.cs
@@ -1,14 +1,64 @@
-﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class NewBehaviourScript1 : MonoBehaviour {
 	int i=0;
+	public Image img;
 	int inc = 1;
 	public Transform[] targets;
 	public float speed;
+	private float speedo;
 	public GameObject player;
-
+	ArrayList ar=new ArrayList();
+	void Start(){
+		speedo = speed;
+		
+	}
 	void Update() {
+		ar.Clear ();	
+		RaycastHit hit;
+		for (int i = -2; i < 3; i++) {
+			Vector3 forward = transform.TransformDirection (Vector3.forward+new Vector3(i*0.25f,0,0))*100;
+			Debug.DrawRay (transform.position, forward, Color.green);
+			ar.Add (forward);
+		}
+		for (int i = -2; i < 3; i++) {
+			Vector3 forward = transform.TransformDirection (Vector3.forward+new Vector3(i*0.25f,1,0))*100;
+			Debug.DrawRay (transform.position, forward, Color.green);
+			ar.Add (forward);
+		}
+		for (int i = -2; i < 3; i++) {
+			Vector3 forward = transform.TransformDirection (Vector3.forward+new Vector3(i*0.25f,-1,0))*100;
+			Debug.DrawRay (transform.position, forward, Color.green);
+			ar.Add (forward);
+		}
+		for (int i = -2; i < 3; i++) {
+			Vector3 forward = transform.TransformDirection (Vector3.forward+new Vector3(i*0.25f,0.5f,0))*100;
+			Debug.DrawRay (transform.position, forward, Color.green);
+			ar.Add (forward);
+		}
+		for (int i = -2; i < 3; i++) {
+			Vector3 forward = transform.TransformDirection (Vector3.forward+new Vector3(i*0.25f,-0.5f,0))*100;
+			Debug.DrawRay (transform.position, forward, Color.green);
+			ar.Add (forward);
+		}
+		speed = speedo;
+		for (int i = 0; i < 19; i++) {
+
+			Vector3 temp = (Vector3)ar [i];
+			if (Physics.Raycast (transform.position, temp, out hit, 100)) {
+				Debug.DrawRay (transform.position, temp, Color.green);
+				if (hit.collider.gameObject.name == "PlayerModel") {
+					speed = 0;
+				}
+
+
+
+
+
+			}
+		}
 			
 		float step = speed * Time.deltaTime;
 		Vector3 targetDir = targets[i].position - transform.position;
