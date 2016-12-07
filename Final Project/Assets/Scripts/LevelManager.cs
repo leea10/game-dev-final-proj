8fa609fe624333efde0ec168d180065814336d28
Ariel Lee <leea10@rpi.edu>
2016-12-06 20:16:59 -0500

    Score display works

--- Final Project/Assets/Scripts/LevelManager.cs
+++ Final Project/Assets/Scripts/LevelManager.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class LevelManager : MonoBehaviour {
 	// Event dispatcher for player progress events.
 	// When the playuer claims one objective.
-	public delegate void ObjectiveCompleteAction(string areaName);
+	public delegate void ObjectiveCompleteAction(int treesMarked);
 	public static ObjectiveCompleteAction OnObjectiveComplete;
 	// When the player completes the entire level.
 	public delegate void LevelCompleteAction();
@@ -13,13 +13,14 @@ public class LevelManager : MonoBehaviour {
 
 	// The areas to scout in the level.
 	// (Mapping: Area name -> has been scouted?)
-	Dictionary<string, bool> scoutAreas;
+	//Dictionary<string, bool> scoutAreas;
 
 	// The trees to mark in the leve.
 	int totalTrees;
 	int treesMarked = 0;
 
 	void Start () {
+		/*
 		// Initialize variables.
 		scoutAreas = new Dictionary<string, bool> ();
 		// Find all scout areas in the level.
@@ -31,6 +32,7 @@ public class LevelManager : MonoBehaviour {
 			scoutAreas.Add (areaName, false);
 			Debug.Log ("[LevelManager Init] Player must scout " + areaName);
 		}
+		*/
 		// Find all trees that need to be marked.
 		totalTrees = GameObject.FindGameObjectsWithTag("landmark").Length;
 		Debug.Log ("[LevelManager Init] Player must mark " + totalTrees + " trees.");
@@ -38,16 +40,17 @@ public class LevelManager : MonoBehaviour {
 
 	void OnEnable () {
 		// Event Listeners.
-		ScoutArea.OnPlayerCollide += ScoutAreaHandler; // Player enters a scout area.
+		//ScoutArea.OnPlayerCollide += ScoutAreaHandler; // Player enters a scout area.
 		Landmark.OnTreeMark += MarkTreeHandler;
 	}
 
 	void OnDisable () {
 		// Stop listening to events.
-		ScoutArea.OnPlayerCollide -= ScoutAreaHandler;
+		//ScoutArea.OnPlayerCollide -= ScoutAreaHandler;
 		Landmark.OnTreeMark -= MarkTreeHandler;
 	}
 
+	/*
 	// Event handler for when player enters a scout area.
 	void ScoutAreaHandler (string areaName) {
 		// If the area is a required area that hasn't been scouted yet, note that the player just scouted it.
@@ -59,11 +62,17 @@ public class LevelManager : MonoBehaviour {
 			}
 		}
 	}
+	*/
 
 	// Event handler for when player marks a tree.
 	void MarkTreeHandler() {
 		treesMarked++;
 		Debug.Log ("[LevelManager OnLandmarkComplete] Player marked tree " + treesMarked + "/" + totalTrees);
+		if (OnObjectiveComplete != null) {
+			OnObjectiveComplete (treesMarked);
+		} else {
+			Debug.Log ("[LevelManager OnObjectiveComplete] No event delegates!");
+		}
 		if (treesMarked == totalTrees) {
 			if (OnLevelComplete != null) {
 				Debug.Log ("[LevelManager OnLevelComplete] Player completed the level");
